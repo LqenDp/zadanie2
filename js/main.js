e8a82232fb690781c674fc66b60238161f8edda1
LqenDp <oorzaksancaj7@gmail.com>
2026-03-14 08:35:53 +0700

    form

--- js/main.js
+++ js/main.js
@@ -2,8 +2,19 @@ let app = new Vue({
     el: '#app',
     data: {
         appName: 'Мои заметки',
+        newCardTitle: '',      
+        newCardTasks: '',  
         column1Count: 0,
         column2Count: 0,
         column3Count: 0
+    },
+    methods: {
+        addCard() {
+            console.log('Заголовок:', this.newCardTitle);
+            console.log('Задачи:', this.newCardTasks);
+            
+            this.newCardTitle = '';
+            this.newCardTasks = '';
+        }
     }
 })
